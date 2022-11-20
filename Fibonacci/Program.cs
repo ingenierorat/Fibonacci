539d00c829ce2c8caceb5afd25c33fb042fdd13e
RAFAEL AUGUSTO TORRES PAULINO <rtorrespaulino@outlook.com>
2022-11-20 10:18:16 -0400

    Imprime el Fibonacci de un numero

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int val;
+            Console.WriteLine("Intoduce el valor");
+            val = int.Parse(Console.ReadLine());
+
+            int[] arreglo = new int[val];
+            arreglo[0] = 0;
+            arreglo[1] = 1;
+
+            for (int i = 2; i < val; i++)
+            {
+                arreglo[i] = arreglo[i - 1] + arreglo[i - 2];
+            }
+            Console.WriteLine(string.Join(",",arreglo));
+            Console.ReadKey();
+
+        }
+    }
+}
